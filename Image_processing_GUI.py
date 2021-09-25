b2599196f0420ff2f51fc14e53ab569e0d2f5469
superyossi23 <86301318+superyossi23@users.noreply.github.com>
2021-09-25 22:05:32 +0900

    Add files via upload

---
+++ Image_processing_GUI.py
@@ -0,0 +1,210 @@
+"""
+
+"""
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as qtc
+import sys
+
+from Image_processing_functions import *
+
+
+class MainWindow(qtw.QWidget):
+
+    page_select = qtc.pyqtSignal([str], ['QObject'])
+
+    def __init__(self):
+        """MainWindow constructor"""
+        super().__init__()
+
+        # Configure the window -------------------------------------------------------
+        self.setWindowTitle('IMAGE PROCESSING APP')
+        self.resize(800, 600)
+
+        # Create out widgets ---------------------------------------------------------
+        self.path_entry = qtw.QLineEdit(cwd, self, maxLength=99)
+        self.dpi_spin = qtw.QSpinBox(self, value=dpi, maximum=300, minimum=100, singleStep=50)
+        self.filename0_entry = qtw.QLineEdit(filename0, self)
+        self.filename1_entry = qtw.QLineEdit(filename1, self)
+        self.format_entry = qtw.QComboBox(self)
+        self.grayscale_check = qtw.QCheckBox('grayscale', self)
+        self.page_off_check = qtw.QCheckBox('page_off', self)
+        self.page_length_entry = qtw.QLineEdit('', self, placeholderText='Int only...')
+        self.separation_spin = qtw.QSpinBox(self, value=separation)
+        self.page_select_entry = qtw.QLineEdit('', self,  placeholderText='Enter page numbers...', maxLength=50)
+
+        self.pdf2image_btn = qtw.QPushButton(
+            'pdf2image',
+            clicked=self.pdf2image_exe
+        )
+        self.pdf2image_dir_btn = qtw.QPushButton(
+            'pdf2image_dir',
+            clicked=self.pdf2image_dir_exe
+        )
+        self.add_image_tiff_btn = qtw.QPushButton(
+            'add_image_tiff',
+            clicked=self.add_image_tif_exe
+        )
+
+        self.pageSelectBtn = qtw.QPushButton('PageSelect', clicked=self.onPageSelect)
+
+        # Configure widgets -------------------------------------------------------------
+        # Add event categories
+        self.format_entry.addItems(
+            ['TIFF', 'JPEG', 'PNG']
+        )
+
+        # Arrange the widgets -----------------------------------------------------------
+        # Create main_layout
+        main_layout = qtw.QHBoxLayout()
+        self.setLayout(main_layout)
+        # result_layout = xxx
+
+
+        # params_layout
+        params_layout = qtw.QVBoxLayout()
+        main_layout.addLayout(params_layout)
+        params_layout.addWidget(qtw.QLabel('Image_processing_functions.py\n'
+                                           'Image_processing_GUI.py'))
+        # Execute
+        execute_form = qtw.QGroupBox('Execute')
+        params_layout.addWidget(execute_form)
+        # execute_form_layout
+        execute_form_layout = qtw.QGridLayout()
+        execute_form_layout.addWidget(qtw.QLabel('# execute_form', self), 1, 1, 1, 10)
+        execute_form_layout.addWidget(self.pdf2image_btn, 2, 1, 1, 1)
+        execute_form_layout.addWidget(qtw.QLabel('<b># pdf2image()</b>', self), 2, 2, 1, 1)
+        execute_form_layout.addWidget(self.pdf2image_dir_btn, 3, 1, 1, 1)
+        execute_form_layout.addWidget(qtw.QLabel('<b># pdf2image_dir()</b>', self), 3, 2, 1, 1)
+        execute_form_layout.addWidget(self.add_image_tiff_btn, 4, 1, 1, 1)
+        execute_form_layout.addWidget(qtw.QLabel('<b># add_image_tiff()</b>', self), 4, 2, 1, 1)
+        # execute_form_layout.addWidget(self.pageSelectBtn, 3, 1, 1, 1)
+        # execute_form_layout.addWidget(qtw.QLabel('<b># onPageSelect()</b>', self), 3, 2, 1, 1)
+        # Set GridLayout to execute_form_layout
+        execute_form.setLayout(execute_form_layout)
+        # Settings
+        settings_form = qtw.QGroupBox('Settings')
+        params_layout.addWidget(settings_form)
+        # settings_form_layout
+        settings_form_layout = qtw.QGridLayout()
+        settings_form_layout.addWidget(qtw.QLabel('# settings_form', self), 1, 1, 1, 10)
+        settings_form_layout.addWidget(self.path_entry, 2, 1, 1, 9)  # (row, column, row span, column span)
+        settings_form_layout.addWidget(qtw.QLabel('<b># path</b>', self, margin=10), 2, 10, 1, 1)
+        settings_form_layout.addWidget(self.dpi_spin, 3, 1, 1, 1)
+        settings_form_layout.addWidget(qtw.QLabel('<b># dpi</b>', self), 3, 2, 1, 1)
+        settings_form_layout.addWidget(self.format_entry, 3, 3, 1, 1)
+        settings_form_layout.addWidget(qtw.QLabel('<b># format</b>', self), 3, 4, 1, 1)
+        settings_form_layout.addWidget(self.filename0_entry, 4, 1, 1, 3)
+        settings_form_layout.addWidget(qtw.QLabel('<b># filename0</b>', self), 4, 4, 1, 1)
+        settings_form_layout.addWidget(self.filename1_entry, 4, 5, 1, 3)
+        settings_form_layout.addWidget(qtw.QLabel('<b># filename1</b>', self), 4, 8, 1, 1)
+        # Set GridLayout to settings_form_layout
+        settings_form.setLayout(settings_form_layout)
+        # Optional
+        optional_form = qtw.QGroupBox('Optional')
+        params_layout.addWidget(optional_form)
+        # optional_form_layout
+        optional_form_layout = qtw.QGridLayout()
+        optional_form_layout.addWidget(qtw.QLabel('# optional_form', self), 1, 1, 1, 10)
+        optional_form_layout.addWidget(self.grayscale_check, 2, 1, 1, 1)
+        optional_form_layout.addWidget(self.page_off_check, 2, 3, 1, 1)
+        optional_form_layout.addWidget(self.page_length_entry, 3, 1, 1, 1)
+        optional_form_layout.addWidget(qtw.QLabel('<b># page_length</b>', self), 3, 2, 1, 1)
+        optional_form_layout.addWidget(self.separation_spin, 3, 3, 1, 1)
+        optional_form_layout.addWidget(qtw.QLabel('<b># separation</b>', self), 3, 4, 1, 1)
+        optional_form_layout.addWidget(self.page_select_entry, 3, 5, 1, 2)
+        optional_form_layout.addWidget(qtw.QLabel('<b># page_select</b>', self), 3, 7, 1, 1)
+        # Set GridLayout to optional_form_layout
+        optional_form.setLayout(optional_form_layout)
+
+        # Connect Events --------------------------------------------------------------
+
+        # grayscale_check -> True
+        self.grayscale_check.setChecked(True)
+        # page_off_check -> True
+        self.page_off_check.setChecked(True)
+        self.page_off_check.toggled.connect(self.page_length_entry.setDisabled)
+        self.page_off_check.toggled.connect(self.separation_spin.setDisabled)
+        self.page_off_check.toggled.connect(self.page_select_entry.setDisabled)
+        # page_length_entry -> False
+        self.page_length_entry.setDisabled(True)
+        # separation_spin -> False
+        self.separation_spin.setDisabled(True)
+        # page_select_entry -> False
+        self.page_select_entry.setDisabled(True)
+        # On going
+        self.pageSelectBtn.setDisabled(True)
+
+        # End main UI code
+        self.show()
+
+    # Slots
+    def onPageSelect(self):
+        # self.page_select[str].emit(self.page_select_entry.text())
+        print('type:', type(self.page_select))
+        print('page_select:', self.page_select_entry.text())
+
+    # Functions
+    def pdf2image_exe(self):
+        # self.page_select[str].connect()
+        # text = self.page_select_entry.text()
+        # print('text:', text)
+        # text = text.replace(' ', '')
+        # intlist = [int(x) for x in text]
+        # self.page_select[str].emit(intlist)
+        # LOG #
+        print('path:', self.path_entry.text())
+        print('dpi:', self.dpi_spin.text())
+        print('filename:', self.filename0_entry.text())
+        print('format:', self.format_entry.currentText())
+        print('page_off:', self.page_off_check.isChecked())
+        print('page_length:', self.page_length_entry.text())
+        print('separation:', self.separation_spin.text())
+        print('grayscale:', self.grayscale_check.isChecked())
+
+        pdf2image(
+            path=self.path_entry.text(),
+            dpi=self.dpi_spin.text(),
+            filename=self.filename0_entry.text(),
+            format=self.format_entry.currentText(),
+            page_off=self.page_off_check.isChecked(),
+            page_length=self.page_length_entry.text(),
+            separation=self.separation_spin.text(),
+            page_select=self.page_select_entry.text(),
+            grayscale=self.grayscale_check.isChecked()
+        )
+
+    def pdf2image_dir_exe(self):
+        # LOG #
+        print('path:', self.path_entry.text())
+        print('dpi:', self.dpi_spin.text())
+        print('format:', self.format_entry.currentText())
+        print('page_off:', self.page_off_check.isChecked())
+        print('grayscale:', self.grayscale_check.isChecked())
+
+        pdf2image_dir(
+            path=self.path_entry.text(),
+            dpi=self.dpi_spin.text(),
+            format=self.format_entry.currentText(),
+            page_off=self.page_off_check.isChecked(),
+            grayscale=self.grayscale_check.isChecked()
+        )
+
+    def add_image_tif_exe(self):
+        add_image_tif(
+            job_dir=self.path_entry.text(),
+            filename0=self.filename0_entry.text(),
+            filename1=self.filename1_entry.text(),
+            grayscale=self.grayscale_check.isChecked()
+        )
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    mw = MainWindow()
+    sys.exit(app.exec())
+
+
+
+
+
---
+++ Image_processing_functions.py
@@ -0,0 +1,435 @@
+"""
+def image_output: Outputs image
+def pdf2image: Convert a PDF to IMAGEs
+def pdf2image_dir: Convert all pdf to image in the dir.
+def saveTiffStack: Save multi-frame tiff file
+def add_image: Add an img1 to an img0
+def add_image_tif: Add an img1 to an img0 (multi-frame TIFF ver.)
+def add_image_all: Add all the images to the another
+def pdf2png_compare: Execute add_image_all to 2 pdf files
+def pdf2tiff_compare: Execute add_image_tif to 2 pdf files
+def bmp2png: Convert bmp image to png image
+
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import glob
+from pdf2image import convert_from_path
+import os
+import glob
+import cv2 as cv
+
+
+# Global settings -----------------------------------------------------------------
+cwd = os.getcwd()
+wd = 'C:/Users/A/Desktop/pythonProject/opencvProject/pdf2image_compare wd'
+
+job_dir = 'pdf2image_compare wd'
+dpi = 300
+filename0 = 'Receipt Excel'
+filename1 = 'Receipt Python3'
+page_off = True
+page_length = 10
+separation = 10
+page_select = [1, 1]
+grayscale = True
+
+# ---------------------------------------------------------------------------------
+
+def image_output(
+        pages, outputDir='some directory', format='TIFF', filename='output file name'
+):
+    """
+    :param pages:
+    :param outputDir:
+    :param format:
+    :param filename: Select output file name
+    :return:
+    """
+    # JPEG #
+    if format == 'JPEG':
+        cnt = 0
+        for page in pages:
+            myfile = outputDir + '/' + filename + str(cnt) + '.jpg'
+            cnt += 1
+            page.save(myfile, 'JPEG')
+            print('Output: ', myfile)
+    # PNG #
+    elif format == 'PNG':
+        cnt = 0
+        for page in pages:
+            myfile = outputDir + '/' + filename + str(cnt) + '.png'
+            cnt += 1
+            page.save(myfile, 'PNG')
+            print('Output: ', myfile)
+    # TIFF #
+    elif format == 'TIFF':
+        myfile = outputDir + '/' + filename + '.tif'
+        pages[0].save(myfile, 'TIFF', compression='tiff_deflate', save_all=True, append_images=pages[1:])
+        print('Output: ', myfile)
+
+
+def pdf2image(
+    path='pdf2image_compare wd',
+    dpi=300,
+    filename='Receipt Python3',
+    format='TIFF',
+    page_off=False,
+    page_length=1,
+    separation=10,
+    page_select=[1],
+    grayscale=False
+):
+    """
+    :param path: PDF file path
+    :param dpi: Select resolution
+    :param filename: Input filename
+    :param format: 'TIFF'/'PNG'/'JPEG'
+    :param page_off: True/False. Whether manage pages or not.
+    :param page_length: int. False. Convert every pages
+    :param separation: Divide all pages into the selected number of pages
+    :param page_select: List object. Select necessary pages. page_off must be False.
+           odd: first_page, even: last_pages. e.g. [1,2, 4,6, 9,9]
+    :return:
+    """
+    # Settings #
+    path = path.replace('\\', '/')
+    pdf_path = path + '/' + filename + '.pdf'
+    # Create ppm dir
+    ppmData = path+'/PPM files/'
+    if not page_off:
+        quotient = page_length // separation
+        remainder = page_length % separation
+    # Create dir if path does not exist
+    if not os.path.exists(path):
+        os.mkdir(path)
+    if not os.path.exists(ppmData):
+        os.mkdir(ppmData)
+
+    # if page_off == True ----------------------------------------------------------
+    if page_off:
+        print('All pdf pages will be converted')
+        # Convert #
+        pages = convert_from_path(pdf_path=pdf_path, dpi=dpi, output_folder=ppmData, grayscale=grayscale)
+        # Output #
+        image_output(pages, path, format, filename)
+
+    # if page_off == False ---------------------------------------------------------
+    elif not page_off:
+        print('page_length =', page_length)
+        # Quotient
+        for j in range(quotient):
+            # Convert #
+            pages = convert_from_path(pdf_path=pdf_path, dpi=dpi, output_folder=ppmData,
+                                      first_page=j * separation + 1, last_page=(j + 1) * separation)
+            # Output #
+            image_output(pages, path, format,
+                         filename+'_'+str(j*separation+1)+'_'+str((j+1)*separation))
+        # Remainder
+        # Convert #
+        if remainder != 0:
+            pages = convert_from_path(pdf_path=pdf_path, dpi=dpi, output_folder=ppmData,
+                                      first_page=j * separation + 1, last_page=(j + 1) * separation + remainder)
+
+            # Output #
+            image_output(pages, path, format,
+                         filename + '_' + str((j+1) * separation + 1) + '_' + str((j + 1) * separation + remainder))
+
+    else:
+        raise Exception('page_length must be int')
+
+
+def pdf2image_dir(
+    path='pdf2image_compare wd',
+    dpi=300,
+    format='TIFF',
+    page_off=True,
+    grayscale=False
+):
+    """
+    Convert all pdf to image in the dir.
+    :param path: PDF file path
+    :param dpi: Select resolution
+    :param format: 'TIFF'/'PNG'/'JPEG'
+    :param page_off: True/False. Whether manage pages or not.
+    """
+    # Settings #
+    # path = path.replace('\\', '/')
+    filelist = os.listdir(path)
+    filelist = list(filter(lambda x: x.endswith('.pdf'), filelist))
+    # Create ppm dir
+    ppmData = path+'/PPM files/'
+    # Create dir if path does not exist
+    if not os.path.exists(path):
+        os.mkdir(path)
+    if not os.path.exists(ppmData):
+        os.mkdir(ppmData)
+
+    # if page_off == True ----------------------------------------------------------
+    if page_off:
+        for i in range(len(filelist)):
+            print('All pdf pages will be converted')
+            # Convert #
+            pages = convert_from_path(pdf_path=path+'\\'+filelist[i], dpi=dpi, output_folder=ppmData, grayscale=grayscale)
+            # Output #
+            image_output(pages, path, format, filelist[i].split('.pdf')[0])
+
+    else:
+        raise Exception('Only work when page_off=True')
+
+
+def saveTiffStack(
+        save_path='pdf2image_compare wd/saveTiffStack result tiff.tif',
+        imgs: 'list' = None
+):
+    stack = []
+    for img in imgs:
+        stack.append(Image.fromarray(img))
+    stack[0].save(save_path, compression='tiff_deflate', save_all=True, append_images=stack[1:])
+
+
+def add_image(
+        job_dir='pdf2image_compare wd/image/',
+        filename0='Receipt Excel',  # pdf, filename0 must be bigger than filename1
+        filename1='Receipt Python3', # pdf
+        out_filename='add_image result',
+):
+    """
+    Add img1 on img0
+    """
+    # Read
+    img0 = cv.imread(job_dir + filename0 + '.jpg')  # flags=-1: alpha mode
+    img1 = cv.imread(job_dir + filename1 + '.jpg')  # flags=-1: alpha mode
+
+    # img0 => img0_r
+    img0_r = np.zeros(img0.shape, dtype='uint8')
+    img0gray = cv.cvtColor(img0, cv.COLOR_BGR2GRAY)
+    ret0, mask0 = cv.threshold(img0gray, 240, 255, cv.THRESH_BINARY)  # White-out
+    img0_r[:, :, :2] = 255  # White BG
+    img0_r[:, :, 2] = mask0  # img0 => img0_r
+
+    # I want to put logo on top-left corner, So I create a ROI (Region of Interest)
+    rows, cols, channels = img1.shape
+    roi = img0_r[0:rows, 0:cols]
+
+    # Now create a mask of img1 and create its inverse mask also
+    img1gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+    ret1, mask1 = cv.threshold(img1gray, 240, 255, cv.THRESH_BINARY)  # White-out
+    mask_inv = cv.bitwise_not(mask1)
+
+    # White-out the BG in ROI
+    img0_bg = cv.bitwise_and(roi, roi, mask=mask1)
+
+    # Take only region of Mask from img1
+    img1_fg = cv.bitwise_and(img1, img1, mask=mask_inv)
+
+    # Put img1 in ROI and modify the img0
+    dst = cv.add(img0_bg, img1_fg)  # ROI
+    img0[0:rows, 0:cols] = dst  # Full Image
+
+    # Output
+    print('Output: ' + cwd + out_filename)
+    cv.imwrite(job_dir + out_filename + '.png', img0)
+
+    # Show
+    cv.imshow('Out', img0)
+    cv.waitKey(0)
+
+    return img0, img1
+
+
+def add_image_tif(
+        job_dir='pdf2image_compare wd/',
+        filename0='Receipt Excel_gray',  # tiff, !filename0 must be bigger than filename1 (pixel size)
+        filename1='Receipt Python3_gray', # tiff
+        grayscale=True
+):
+    """
+    Add img1 on img0. img0 is changed to cyan.
+    :param job_dir:
+    :param filename0:
+    :param filename1:
+    :param grayscale: True/False
+    :return:
+    """
+    f0_list, f1_list = [], []
+    for file in os.listdir(wd):
+        if file.startswith(filename0) and file.endswith('.tif'):
+            f0_list.append(file)
+        if file.startswith(filename1) and file.endswith('.tif'):
+            f1_list.append(file)
+
+    for f0, f1 in zip(f0_list, f1_list):
+
+        out_filename = '{} on {}'.format(f1.split('.')[0], f0.split('.')[0])
+
+        # Read (Multi-frame Tiff file)
+        ret0, imgs0 = cv.imreadmulti(job_dir + f0)
+        ret1, imgs1 = cv.imreadmulti(job_dir + f1)
+
+        cnt = 0
+        for img0, img1 in zip(imgs0, imgs1):
+
+            # img0 => img0_color
+            # Convert gray to RGB (img0)
+            img0_color = np.zeros((img0.shape[0], img0.shape[1], 3), dtype='uint8')
+
+            if grayscale:
+                ret0, mask0 = cv.threshold(img0, 240, 255, cv.THRESH_BINARY)  # white -> out
+                ret1, mask1 = cv.threshold(img1, 240, 255, cv.THRESH_BINARY)  # white -> out
+
+                rows, cols = img1.shape
+
+            else:
+                img0gray = cv.cvtColor(img0, cv.COLOR_BGR2GRAY)
+                img1gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+                ret0, mask0 = cv.threshold(img0gray, 240, 255, cv.THRESH_BINARY)  # white -> out
+                ret1, mask1 = cv.threshold(img1gray, 240, 255, cv.THRESH_BINARY)  # white -> out
+
+                rows, cols, channels = img1.shape
+
+            img0_color[:, :, 0:] = 255  # Black to White
+            img0_color[:, :, 0] = mask0  # White to Color  # Color select is here
+
+            # Create a ROI (Region of Interest)
+            roi = img0_color[0:rows, 0:cols]
+
+            # White-out the BG in ROI
+            img1_on_img0 = cv.bitwise_and(roi, roi, mask=mask1)
+            # Turned out to be unnecessary lines
+            # img0_bg = cv.bitwise_and(roi, roi, mask=mask1)
+            # Take only region of Mask from img1
+            # mask_inv = cv.bitwise_not(mask1)
+            # img1_fg = cv.bitwise_and(img1, img1, mask=mask_inv)
+            # Put img1 in ROI and modify the img0
+            # dst = cv.add(img0_bg, img1_fg)  # ROI
+            # img0[0:rows, 0:cols] = dst  # Full Image
+            imgs0[cnt] = img1_on_img0
+            cnt += 1
+
+        # Output #
+        print('Output: ', job_dir + '\\' + out_filename + '.tif')
+        saveTiffStack(save_path=job_dir + '\\' + out_filename + '.tif', imgs=imgs0)
+
+
+def add_image_all(
+    filename0='Receipt Excel',  # pdf, filename0 must be equal/bigger than filename1
+    filename1='Receipt Python3', # pdf
+    out_filename='add_image_all result',
+    job_dir='pdf2image_compare wd/'
+):
+    """
+    Add img1 on img0 (for PNG/JPEG)
+    """
+    file_dir = job_dir + 'image/'
+    file0s, file1s = [], []
+
+    # Split files to file0s and file1s
+    for filename in os.listdir(file_dir):
+        if filename0 in filename and 'jpg' in filename:
+            file0s.append(filename)
+        elif filename1 in filename and 'jpg' in filename:
+            file1s.append(filename)
+
+    cnt = 0
+    for i in range(len(file0s)):
+
+        # Read
+        img0 = cv.imread(file_dir + file0s[i])  # flags=-1: alpha mode
+        img1 = cv.imread(file_dir + file1s[i])  # flags=-1: alpha mode
+
+        # img0 => img0_r
+        img0_r = np.zeros(img0.shape, dtype='uint8')
+        img0gray = cv.cvtColor(img0, cv.COLOR_BGR2GRAY)
+        ret0, mask0 = cv.threshold(img0gray, 240, 255, cv.THRESH_BINARY)  # White-out
+        img0_r[:, :, :2] = 255  # White BG
+        img0_r[:, :, 2] = mask0  # img0 => img0_r
+
+        # I want to put logo on top-left corner, So I create a ROI (Region of Interest)
+        rows, cols, channels = img1.shape
+        roi = img0_r[0:rows, 0:cols]
+
+        # Now create a mask of img1 and create its inverse mask also
+        img1gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+        ret1, mask1 = cv.threshold(img1gray, 240, 255, cv.THRESH_BINARY)  # White-out
+        mask_inv = cv.bitwise_not(mask1)
+
+        # White-out the BG in ROI
+        img0_bg = cv.bitwise_and(roi, roi, mask=mask1)
+
+        # Take only region of Mask from img1
+        img1_fg = cv.bitwise_and(img1, img1, mask=mask_inv)
+
+        # Put img1 in ROI and modify the img0
+        dst = cv.add(img0_bg, img1_fg)  # ROI
+        img0[0:rows, 0:cols] = dst  # Full Image
+
+        # Output
+        print('Output: ', cwd + file_dir + out_filename + '_' + str(cnt) + '.png')
+        cv.imwrite(job_dir + out_filename + '_' + str(cnt) + '.png', img0)
+
+        cnt += 1
+
+
+def pdf2png_compare(
+        job_dir='pdf2image_compare wd/',
+        filename0='Receipt Excel',
+        filename1='Receipt Python3',
+        page_off=True,
+        page_length=1,
+        separation=10
+):
+    """
+    Execute add_image_all to 2 pdf files
+    """
+
+    # First file #
+    pdf2image(path=job_dir + filename0 + '.pdf', outputDir=job_dir, filename=filename0,
+              page_off=page_off, page_length=page_length, separation=separation)
+
+    # Second file #
+    pdf2image(path=job_dir + filename1 + '.pdf', outputDir=job_dir, filename=filename1,
+              page_off=page_off, page_length=page_length, separation=separation)
+
+    # Output #
+    add_image_all(filename0=filename0, filename1=filename1, job_dir=job_dir)
+
+
+def pdf2tiff_compare(
+        job_dir='pdf2image_compare wd/',
+        filename0='Receipt Excel',
+        filename1='Receipt Python3',
+        page_off=True,
+        page_length=1,
+        separation=10,
+        grayscale=True,
+):
+    # First file #
+    pdf2image(path=job_dir + filename0 + '.pdf', outputDir=job_dir, filename=filename0, format='TIFF',
+              page_off=page_off, page_length=page_length, separation=separation,
+              grayscale=grayscale)
+
+    # Second file #
+    pdf2image(path=job_dir + filename1 + '.pdf', outputDir=job_dir, filename=filename1, format='TIFF',
+              page_off=page_off, page_length=page_length, separation=separation,
+              grayscale=grayscale)
+
+    # Output #
+    add_image_tif(job_dir=job_dir, filename0=filename0, filename1=filename1, grayscale=grayscale)
+
+
+def bmp2png(out_dir=''):
+    """
+    out_dir: output directory name
+    """
+    cnt = 0
+    for img in glob.glob('images/*.bmp'):
+        Image.open(img).resize((300,300)).save(os.path.join(out_dir, str(cnt) + '.png'))
+        cnt += 1
+
+
+
+
+
+
