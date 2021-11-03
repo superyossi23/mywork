1d12cc3ee24a94cd2f4188fdc8718157050687c8
superyossi23 <86301318+superyossi23@users.noreply.github.com>
2021-11-03 09:20:45 +0900

    3 columns ver. added

--- Image_comparator_inHtml.py
+++ Image_comparator_inHtml.py
@@ -1,15 +1,18 @@
 from webbrowser import open_new_tab
 import os
+import sys
 from htmlModule import *
 
 
 # SETTINGS
 wd = 'C:/Users/A/Desktop/stock_data'
-cwd = os.getcwd()
-filelist = os.listdir(wd)
-filelist = list(filter(lambda x: x.endswith('.png'), filelist))
-out_file = wd + '/' + 'Image_comparator.html'
-tag_name = 'Comparator'
+cwd = sys.path[-1]
+filelist = os.listdir(wd)  # Work file directory
+filelist = list(filter(lambda x: x.endswith('.png'), filelist))  # Work file
+out_file = cwd  # Output file directory
+out_filename = 'Image_comparator.html'  # Output file name
+tab_name = 'Image Comparator'  # Tab name for html file
+col_num = 3  # The number of columns
 
 # Read html base file
 # temp_wrapper = 'temp_wrapper.html'
@@ -29,42 +32,90 @@ base = """
 </html>
 """
 
-# Repeat pattern
-wrapper = """
-<tr>
-  <td>%s<img src=%s></td>
-  <td>%s<img src=%s></td>
-</tr>
-"""
-
 body = ''
-# 2 COLUMNS VER.
-# In the case of 1 column missing
-if len(filelist)%2 == 1:
-    filelist.append('None')
-# Add wrapper-str to <body>
-for i in range(len(filelist)//2):
-    body = body + wrapper
-# Insert content to <table>
-index = base.find('</table>')
-base = base[:index] + body + base[index:]
+# 2 COLUMNS VER. -----------------------------------------------------------
+if col_num == 2:
+    # Repeat pattern
+    wrapper = """
+    <tr>
+      <td>%s<img src=%s></td>
+      <td>%s<img src=%s></td>
+    </tr>
+    """
+    # In the case of 1 column missing
+    if len(filelist)%2 == 1:
+        filelist.append('None')
+    # Add wrapper (str) to <body>
+    for i in range(len(filelist)//2):
+        body = body + wrapper
+    # Insert content to <table>
+    index = base.find('</table>')
+    base = base[:index] + body + base[index:]
 
-# Divide filelist into 2 filelists
-# Upper/Lower
-filelist0 = filelist[:len(filelist)//2]
-filelist1 = filelist[len(filelist)//2:]
-# Odd/Even
-# filelist0, filelist1 = get_oddeven(filelist)
+    # Divide filelist into 2 filelists
+    # Upper/Lower
+    filelist0 = filelist[:len(filelist)//2]
+    filelist1 = filelist[len(filelist)//2:]
+    # Odd/Even
+    # filelist0, filelist1 = get_oddeven(filelist)
 
-# Make a tuple for wrapper
-wrap_info = [tag_name]
-for f0,f1 in zip(filelist0, filelist1):
-    wrap_info.append(f0)  # Title
-    wrap_info.append(f0)  # Image
-    wrap_info.append(f1)  # Title
-    wrap_info.append(f1)  # Image
-# List to tuple (for wrapper)
-wrap_info = tuple(wrap_info)
+    # Make a tuple for wrapper
+    wrap_info = [tab_name]
+    for f0,f1 in zip(filelist0, filelist1):
+        wrap_info.append(f0)  # Title
+        wrap_info.append(wd + '/' + f0)  # Image
+        wrap_info.append(f1)  # Title
+        wrap_info.append(wd + '/' + f1)  # Image
+    # List to tuple (for wrapper)
+    wrap_info = tuple(wrap_info)
+
+# 3 COLUMNS VER. -----------------------------------------------------------
+elif col_num == 3:
+    # Repeat pattern
+    wrapper = """
+    <tr>
+      <td>%s<img src=%s></td>
+      <td>%s<img src=%s></td>
+      <td>%s<img src=%s></td>
+    </tr>
+    """
+    # In the case of 1 column missing
+    if len(filelist) % 3 == 1:
+        filelist.append('None')
+        filelist.append('None')
+    # In the case of 2 column missing
+    elif len(filelist) % 3 == 2:
+        filelist.append('None')
+    # Add wrapper (str) to <body>
+    for i in range(len(filelist)//3):
+        body = body + wrapper
+    # Insert content to <table>
+    index = base.find('</table>')
+    base = base[:index] + body + base[index:]
+
+    # Divide filelist into 3 filelists
+    # Upper/Middle/Lower
+    filelist0 = filelist[:len(filelist) * 1 // 3]
+    filelist1 = filelist[len(filelist) * 1 // 3:len(filelist) * 2 // 3]
+    filelist2 = filelist[len(filelist) * 2 // 3:]
+    # Remainder == 0/1/2
+    # TBD
+
+    # Make a tuple for wrapper
+    wrap_info = [tab_name]
+    for f0,f1,f2 in zip(filelist0, filelist1, filelist2):
+        wrap_info.append(f0)  # Title
+        wrap_info.append(wd + '/' + f0)  # Image
+        wrap_info.append(f1)  # Title
+        wrap_info.append(wd + '/' + f1)  # Image
+        wrap_info.append(f2)  # Title
+        wrap_info.append(wd + '/' + f2)  # Image
+    # List to tuple (for wrapper)
+    wrap_info = tuple(wrap_info)
+
+else:
+    print('wrap_info was not created. Undefined col_num selected.')
+# --------------------------------------------------------------------------
 
 # WRAPPER
 main = base % wrap_info
@@ -75,10 +126,10 @@ main = base % wrap_info
 #                   )
 
 # OUTPUT
-f = open(out_file, 'w')
+f = open(out_file + '/' + out_filename, 'w')
 f.write(main)
 f.close()
 
 # SHOW
-open_new_tab(out_file)
+open_new_tab(out_file + '/' + out_filename)
 
