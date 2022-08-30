39d07ae7b91c3ff6ae04473164e518451772a265
superyossi23 <86301318+superyossi23@users.noreply.github.com>
2022-08-31 06:54:52 +0900

    Add files via upload

--- create_waveform.py
+++ create_waveform.py
@@ -9,20 +9,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-n = np.linspace(0, 400*10**-6, 500)
-pulse_freq = 20*10**3  # us
-amplitude = 10  # V
-noise_pulse = signal.square(np.pi * pulse_freq * n, duty=10**-2) * amplitude + amplitude
+n = np.linspace(0, 1000, 1000)  # 0 to 1000 us sampled at 1000 MHz
 
-pulse_freq = 5 * 10**3  # us
+# NOISE
+pulse_freq = 20  # kHz
+amplitude = 10  # V
+noise_pulse = signal.square(2 * np.pi * pulse_freq * n, duty=0.8*10**-2) * amplitude + amplitude
+
+# SIGNAL
+pulse_freq = 5  # kHz
 amplitude = 2.5  # V
 signal_pulse = signal.square(2 * np.pi * pulse_freq * n) * amplitude + amplitude
 
-signal = noise_pulse + signal_pulse
+# SAVING WAVEFORM
+n = n * 10**-6  # us
+signal = (noise_pulse + signal_pulse)
 
-# save_file = np.array([n, signal])
-# np.savetxt('create_waveform.csv', save_file, delimiter=',')
+# SAVE
 save_file = pd.DataFrame([n, signal])
 save_file = save_file.T
-save_file.to_csv('create_waveform.csv', header=False, index=False)
+# save_file.to_csv('create_waveform.csv', header=False, index=False)
+
+print('create_waveform.py done!!\ncreate_waveform.csv was saved.')
 
